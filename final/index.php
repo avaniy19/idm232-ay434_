16e1a59e170ed2ad2cba763a53256e4515ad612e
avani <ay434@drexel.edu>
2022-10-12 23:16:52 -0400

    assignment-01

---
+++ final/index.php
@@ -0,0 +1,8 @@
+<?php include_once 'components/header.php'; ?>
+
+<?php $title = 'My IDM BLEHHHHH'; ?>
+
+<h1> <?php echo $title; ?> </h1>
+<!-- <script> put javascript inside here main.js </script> -->
+
+<?php include_once 'components/footer.php'; ?>
