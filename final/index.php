ee3d6c0630c9ffce58ff252aa81e91101dafaadf
avani <ay434@drexel.edu>
2022-11-10 01:44:30 -0500

    final

--- final/index.php
+++ final/index.php
@@ -1,8 +1,55 @@
 <?php include_once 'components/header.php'; ?>
 
-<?php $title = 'Assignment - 01'; ?>
+<!-- <?php $title = 'Assignment - 01'; ?>
 
-<h1> <?php echo $title; ?> </h1>
+<h1> <?php echo $title; ?> </h1> -->
 <!-- <script> put javascript inside here main.js </script> -->
 
-<?php include_once 'components/footer.php'; ?>
+<div class="home__container">
+
+    <div class="home__title title">
+        <h1>your dream cake awaits...</h1>
+    </div>
+
+    <div class="home__left">
+        <div class="images__left home__images">
+            <div class="item__left"></div>
+            <div class="item__left"></div>
+            <div class="item__left"></div>
+        </div>
+
+        <div class="home__browse">
+            <div class="browse__caption">
+                <h3>let’s make your dreams come true!
+                    browse our award-winning cake selection.
+                </h3>
+            </div>
+
+            <div class="browse__button">
+
+                <h4>
+                    <i class="material-icons">cake</i>
+                    browse
+                </h4>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="home__line"></div>
+
+    <div class="home__right">
+        <div class="images__right home__images">
+
+        </div>
+
+        <div class="home__customize">
+
+        </div>
+    </div>
+
+
+
+
+    <?php include_once 'components/footer.php'; ?>
