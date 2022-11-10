33b0752ce6e1952500fc226df1748b174ede81a6
avani <ay434@drexel.edu>
2022-11-10 18:38:52 -0500

    code

--- final/components/header.php
+++ final/components/header.php
@@ -5,13 +5,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bellota:ital,wght@1,700&family=Montserrat+Alternates:ital,wght@1,400&family=Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bellota:ital,wght@1,700&family=Montserrat+Alternates:ital,wght@1,400&family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 
     <title>IDM 232 Final Project</title>
 </head>
 
 <body>
+
+    <div class="topnav" id="myTopnav">
+
+        <a href="" class="active">LOGO</a>
+
+        <div class="menu">
+            <a href="" class="menu__item">our cakes</a>
+            <a href="" class="menu__item">custom order</a>
+            <a href="" class="menu__item">about us</a>
+            <a href="" class="menu__item">log in</a>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+
+
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>
--- final/index.php
+++ final/index.php
@@ -5,7 +5,7 @@
 <h1> <?php echo $title; ?> </h1> -->
 <!-- <script> put javascript inside here main.js </script> -->
 
-<div class="home__container">
+<!-- <div class="home__container">
 
     <div class="home__title title">
         <h1>your dream cake awaits...</h1>
@@ -25,11 +25,11 @@
                 </h3>
             </div>
 
-            <div class="browse__button">
+            <div class="browse__button button">
 
                 <h4>
                     <i class="material-icons">cake</i>
-                    browse
+                    <div class="button__title">browse</div>
                 </h4>
             </div>
 
@@ -41,15 +41,72 @@
 
     <div class="home__right">
         <div class="images__right home__images">
-
+            <div class="item__right"></div>
+            <div class="item__right"></div>
+            <div class="item__right"></div>
         </div>
 
         <div class="home__customize">
+            <div class="customize__caption">
+                <h3>dare to dream a little bigger?
+                    customize your own cake and get a quote today.
+                </h3>
+            </div>
+
+            <div class="customize__button button">
+
+                <h4>
+                    <i class="material-icons">cake</i>
+                    <div class="button__title">customize</div>
+
+                </h4>
+            </div>
 
         </div>
+    </div> -->
+
+<div class="container">
+    <div class="recipe__title">
+        <h1>classic birthday cake</h1>
     </div>
 
+    <div class="recipe__container">
+        <div class="div1">
+            <img class="recipe__images" src="components/images/classiccake1.png">
+            <img class="recipe__images" src="components/images/classiccake2.png">
+            <img class="recipe__images" src="components/images/classiccake3.png">
+
+        </div>
+        <div class="div2">
+            <h2>about</h2>
+            <p>4 layers of moist buttery vanilla pound cake,
+                creamy vanilla bean buttercream, white chocolate drizzle,
+                fluffy vanilla whipped cream swirls and shaved white chocolate curls!</p>
+        </div>
+        <div class="div3"></div>
+        <div class="div4">
+            <h2>ingredients</h2>
+            <ul>
+                <li>cake flour</li>
+                <li>unsalted butter</li>
+                <li>vanilla pudding</li>
+                <li>sugar</li>
+                <li>powdered sugar</li>
+                <li>eggs</li>
+                <li>vegetable oil</li>
+                <li>buttermilk</li>
+                <li>non-dairy creamer</li>
+                <li>white chocolate</li>
+                <li>baking powder</li>
+                <li>salt</li>
+                <li>vanilla bean paste</li>
+                <li>20x vanilla extract</li>
+            </ul>
+        </div>
+    </div>
+</div>
 
 
 
-    <?php include_once 'components/footer.php'; ?>
+
+<?php include_once 'components/footer.php'; ?>
